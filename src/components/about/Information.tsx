06d69d925c3f445b59958ea715706c1384249155
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-17 11:17:51 +0800

    imported 3D

--- src/components/about/Information.tsx
+++ src/components/about/Information.tsx
@@ -1,5 +1,13 @@
+import Spline from "@splinetool/react-spline";
+
 const Information = () => {
-  return <div>Information</div>;
+  return (
+    <div>
+      <div className="flex justify-center items-center">
+        <Spline scene="https://prod.spline.design/0X80iN7OHufLxu8B/scene.splinecode" />
+      </div>
+    </div>
+  );
 };
 
 export default Information;
