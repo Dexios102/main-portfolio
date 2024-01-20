0bbe24db1d57d5cb79d613bc13914d70080891ea
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-20 20:42:31 +0800

    updated

--- src/components/about/Information.tsx
+++ src/components/about/Information.tsx
@@ -2,10 +2,8 @@ import Spline from "@splinetool/react-spline";
 
 const Information = () => {
   return (
-    <div>
-      <div className="flex justify-center items-center">
-        <Spline scene="https://prod.spline.design/WqYap6iM4cfrLSK3/scene.splinecode" />
-      </div>
+    <div className="flex justify-center items-center">
+      <Spline scene="https://prod.spline.design/WqYap6iM4cfrLSK3/scene.splinecode" />
     </div>
   );
 };
