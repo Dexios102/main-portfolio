7f99507a9464eaf14b1126b6b6df9f8eb71215bc
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-20 20:25:31 +0800

    updated 3d

--- src/components/about/Information.tsx
+++ src/components/about/Information.tsx
@@ -4,7 +4,7 @@ const Information = () => {
   return (
     <div>
       <div className="flex justify-center items-center">
-        <Spline scene="https://prod.spline.design/0X80iN7OHufLxu8B/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/WqYap6iM4cfrLSK3/scene.splinecode" />
       </div>
     </div>
   );
