3db51f8841a4d7b9a6df0a74b9d7fe21f9ef904b
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-22 10:10:08 +0800

    information setup

--- src/components/about/Information.tsx
+++ src/components/about/Information.tsx
@@ -2,8 +2,22 @@ import Spline from "@splinetool/react-spline";
 
 const Information = () => {
   return (
-    <div className="flex justify-center items-center">
-      <Spline scene="https://prod.spline.design/WqYap6iM4cfrLSK3/scene.splinecode" />
+    <div className="flex md:flex-row md:flex-start items-center justify-between flex-col flex-end">
+      <div className="text-3xl font-bold text-white">
+        <h1 className="text-black dark:text-white">About Me</h1>
+        <p className="dark:text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nisi quos
+          itaque aspernatur quisquam sed numquam dolorem neque provident saepe
+          eveniet fugiat magni quod sint, quidem voluptatem omnis in nihil
+          distinctio libero quam totam beatae officiis sunt! At, animi harum.
+        </p>
+      </div>
+      <div className="">
+        <Spline
+          className="relative top-0 right-0 m-0"
+          scene="https://prod.spline.design/WqYap6iM4cfrLSK3/scene.splinecode"
+        />
+      </div>
     </div>
   );
 };
