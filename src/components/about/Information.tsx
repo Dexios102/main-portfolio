d0e7c0dacd9b7c6e6369620e9dca91c07b12d7ac
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-23 16:22:36 +0800

    final UI for mobile devices for about about section

--- src/components/about/Information.tsx
+++ src/components/about/Information.tsx
@@ -1,5 +1,6 @@
-import blob from "../../assets/blob.svg";
+import me from "../../assets/me1.png";
 import blobout from "../../assets/blobout.svg";
+import redblack from "../../assets/red_black.png";
 
 const Information = () => {
   return (
@@ -10,7 +11,14 @@ const Information = () => {
       <div className="text-3xl font-bold text-white mt-4">
         <h1 className="text-black dark:text-white text-center mb-8 font-bold">
           {" "}
-          Your Creative Full Stack Developer!
+          Your{" "}
+          <span
+            className="bg-gradient-to-r from-rose-800 via-red-400
+           dark:to-white to-gray-500 bg-clip-text inline-block text-transparent"
+          >
+            Creative
+          </span>{" "}
+          Full Stack Developer!
         </h1>
         <p
           className="dark:text-white text-md border-b-2 border-red-800 pl-2 mb-2 max-w-[40%]
@@ -22,13 +30,36 @@ const Information = () => {
           Hey there! I'm Dexter, a Full Stack Developer from the Philippines.
           Specializing MERN Stack Typescript, I'm currently freelancing and
           focusing on web/software development. My coding journey started in
-          grade 11, and I bring both technical expertise and artistic flair to
-          create modern and visually appealing web applications. Let's
+          Grade 11 JHS, and I bring both technical expertise and artistic flair
+          to create modern and visually appealing web applications. Let's
           collaborate and build something great!
         </p>
-        <div className="relative">
-          <img src={blobout} alt="" className="" />
-          <img src={blob} alt="" className="absolute top-0 left-0" />
+        <div className="relative -z-[1]">
+          <img
+            src={blobout}
+            alt="bloblayer"
+            className="absolute mix-blend-multiply rotate-40"
+          />
+          <img src={redblack} alt="splash" />
+          <img
+            src={me}
+            alt="blob_image"
+            className="absolute top-0 left-0 rounded-full"
+          />
+          <div className="text-black dark:text-white text-center absolute text-sm top-10 right-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl">
+            <span className="text-4xl">5+</span>
+            <br />
+            <span className="text-sm">Years of</span>
+            <br />
+            <span className="text-sm">Coding Exp.</span>
+          </div>
+          <div className="text-black dark:text-white text-center absolute text-sm bottom-28 left-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl">
+            <span className="text-4xl">15+</span>
+            <br />
+            <span className="text-sm">Projects</span>
+            <br />
+            <span className="text-sm">Completed</span>
+          </div>
         </div>
       </div>
     </div>
