5bcee438d18e307a511b8e59567782f7e79dc446
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-23 11:54:07 +0800

    added CV download function

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,10 +1,15 @@
 import { FaFileDownload } from "react-icons/fa";
+import CVFile from "../assets/CV - Dexter Dave Cajayon.pdf";
 
-const DownloadButton = () => {
+const DownloadButton: React.FC = () => {
+  const handleCvDownload = () => {
+    window.open(CVFile, "_blank");
+  };
   return (
     <button
       className="download-button shadow-md shadow-gray-600 dark:shadow-gray-800
     dark:shadow-lg"
+      onClick={handleCvDownload}
     >
       <div className="svg-wrapper-1">
         <div className="svg-wrapper">
