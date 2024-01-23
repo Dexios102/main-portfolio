05fb0aad90894128aea821ac9cd8909ddc4c457a
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-23 21:20:07 +0800

    final responsive, information section

--- src/components/about/Information.tsx
+++ src/components/about/Information.tsx
@@ -7,12 +7,13 @@ import redblack from "../../assets/red_black.png";
 const Information = () => {
   return (
     <div
-      className="flex xl:flex-row items-center justify-between flex-col
+      className="flex md:flex-row md:flex-1/2 items-center justify-between flex-col
     overflow-hidden"
     >
-      <div className="text-3xl font-bold text-white mt-4">
+      <div className="text-3xl font-bold text-white mt-4 basis-1/2">
         <motion.h1
-          className="text-black dark:text-white text-center mb-8 font-bold"
+          className="text-black dark:text-white text-center mb-8 font-bold md:text-left md:text-6xl
+           md:font-extrabold"
           variants={fadeIn("up", 0.4)}
           initial="hidden"
           whileInView={"show"}
@@ -31,12 +32,12 @@ const Information = () => {
             transition={{ duration: 1.5 }}
           >
             Creative
-          </motion.span>{" "}
-          Full Stack Developer!
+          </motion.span>
+          <br className="hidden md:block" /> Full Stack Developer!
         </motion.h1>
         <motion.p
           className="dark:text-white text-md border-b-2 border-red-800 pl-2 mb-2 max-w-[40%]
-        font-bold uppercase"
+        font-bold uppercase md:max-w-[30%] md:mb-6"
           variants={fadeIn("left", 0.5)}
           initial="hidden"
           whileInView={"show"}
@@ -46,7 +47,7 @@ const Information = () => {
           Biography
         </motion.p>
         <motion.p
-          className="dark:text-gray-300 text-md text-center"
+          className="dark:text-gray-300 text-md text-center md:text-left md:max-w-xl"
           variants={fadeIn("up", 0.7)}
           initial="hidden"
           whileInView={"show"}
@@ -60,52 +61,52 @@ const Information = () => {
           to create modern and visually appealing web applications. Let's
           collaborate and build something great!
         </motion.p>
-        <div className="relative -z-[1]">
-          <motion.img
-            src={redblack}
-            alt="splash"
-            variants={scaleUp(1)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.7 }}
-            transition={{ duration: 1.5 }}
-          />
-          <img
-            src={me}
-            alt="blob_image"
-            className="absolute top-0 left-0 rounded-full hover:grayscale"
-          />
-          <motion.div
-            className="text-black dark:text-white text-center absolute text-sm
-           top-10 right-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl"
-            variants={scaleUp(2)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.7 }}
-            transition={{ duration: 1.5 }}
-          >
-            <span className="text-4xl">5+</span>
-            <br />
-            <span className="text-sm">Years of</span>
-            <br />
-            <span className="text-sm">Coding Exp.</span>
-          </motion.div>
-          <motion.div
-            className="text-black dark:text-white text-center absolute text-sm
-           bottom-28 left-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl"
-            variants={scaleUp(2.5)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.7 }}
-            transition={{ duration: 1.5 }}
-          >
-            <span className="text-4xl">15+</span>
-            <br />
-            <span className="text-sm">Projects</span>
-            <br />
-            <span className="text-sm">Completed</span>
-          </motion.div>
-        </div>
+      </div>
+      <div className="relative -z-[1] basis-1/2">
+        <motion.img
+          src={redblack}
+          alt="splash"
+          variants={scaleUp(1)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          transition={{ duration: 1.5 }}
+        />
+        <img
+          src={me}
+          alt="blob_image"
+          className="absolute top-0 left-0 rounded-full hover:grayscale md:w-[32rem] md:left-32 md:top-36"
+        />
+        <motion.div
+          className="text-black dark:text-white text-center absolute text-sm
+           top-10 right-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl md:text-lg md:px-6"
+          variants={scaleUp(2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          transition={{ duration: 1.5 }}
+        >
+          <span className="text-4xl">5+</span>
+          <br />
+          <span className="text-sm">Years of</span>
+          <br />
+          <span className="text-sm">Coding Exp.</span>
+        </motion.div>
+        <motion.div
+          className="text-black dark:text-white text-center absolute text-sm
+           bottom-28 left-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl md:text-lg md:px-6"
+          variants={scaleUp(2.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          transition={{ duration: 1.5 }}
+        >
+          <span className="text-4xl">15+</span>
+          <br />
+          <span className="text-sm">Projects</span>
+          <br />
+          <span className="text-sm">Completed</span>
+        </motion.div>
       </div>
     </div>
   );
