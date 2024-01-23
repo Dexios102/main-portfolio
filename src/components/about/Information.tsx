f3598a49ef44ed195c38c24b44c0cfd10399a1c2
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-23 20:16:07 +0800

    animation

--- src/components/about/Information.tsx
+++ src/components/about/Information.tsx
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion";
+import { fadeIn, scaleUp } from "../../utils/variants";
+
 import me from "../../assets/me1.png";
 import blobout from "../../assets/blobout.svg";
 import redblack from "../../assets/red_black.png";
@@ -9,31 +12,55 @@ const Information = () => {
     overflow-hidden"
     >
       <div className="text-3xl font-bold text-white mt-4">
-        <h1 className="text-black dark:text-white text-center mb-8 font-bold">
+        <motion.h1
+          className="text-black dark:text-white text-center mb-8 font-bold"
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true, amount: 0.7 }}
+          transition={{ duration: 0.5 }}
+        >
           {" "}
           Your{" "}
-          <span
+          <motion.span
             className="bg-gradient-to-r from-rose-800 via-red-400
            dark:to-white to-gray-500 bg-clip-text inline-block text-transparent"
+            variants={scaleUp(1.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: true, amount: 0.7 }}
+            transition={{ duration: 1.5 }}
           >
             Creative
-          </span>{" "}
+          </motion.span>{" "}
           Full Stack Developer!
-        </h1>
-        <p
+        </motion.h1>
+        <motion.p
           className="dark:text-white text-md border-b-2 border-red-800 pl-2 mb-2 max-w-[40%]
         font-bold uppercase"
+          variants={fadeIn("left", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true, amount: 0.7 }}
+          transition={{ duration: 1.5 }}
         >
           Biography
-        </p>
-        <p className="dark:text-gray-300 text-md text-center">
+        </motion.p>
+        <motion.p
+          className="dark:text-gray-300 text-md text-center"
+          variants={fadeIn("up", 0.7)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true, amount: 0.7 }}
+          transition={{ duration: 1.5 }}
+        >
           Hey there! I'm Dexter, a Full Stack Developer from the Philippines.
           Specializing MERN Stack Typescript, I'm currently freelancing and
           focusing on web/software development. My coding journey started in
           Grade 11 JHS, and I bring both technical expertise and artistic flair
           to create modern and visually appealing web applications. Let's
           collaborate and build something great!
-        </p>
+        </motion.p>
         <div className="relative -z-[1]">
           <img
             src={blobout}
@@ -44,22 +71,38 @@ const Information = () => {
           <img
             src={me}
             alt="blob_image"
-            className="absolute top-0 left-0 rounded-full"
+            className="absolute top-0 left-0 rounded-full hover:grayscale"
           />
-          <div className="text-black dark:text-white text-center absolute text-sm top-10 right-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl">
+          <motion.div
+            className="text-black dark:text-white text-center absolute text-sm
+           top-10 right-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl"
+            variants={scaleUp(2.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            transition={{ duration: 1.5 }}
+          >
             <span className="text-4xl">5+</span>
             <br />
             <span className="text-sm">Years of</span>
             <br />
             <span className="text-sm">Coding Exp.</span>
-          </div>
-          <div className="text-black dark:text-white text-center absolute text-sm bottom-28 left-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl">
+          </motion.div>
+          <motion.div
+            className="text-black dark:text-white text-center absolute text-sm
+           bottom-28 left-2 font-bold rounded-lg bg-white/10 p-4 backdrop-blur-2xl"
+            variants={scaleUp(3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            transition={{ duration: 1.5 }}
+          >
             <span className="text-4xl">15+</span>
             <br />
             <span className="text-sm">Projects</span>
             <br />
             <span className="text-sm">Completed</span>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
