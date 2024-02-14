cefa3704b4cddaf80bec53e2ae7f6ebe1e4e6e56
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-02-14 12:51:58 +0800

    Experience final ui

--- src/components/about/Experience.tsx
+++ src/components/about/Experience.tsx
@@ -22,24 +22,27 @@ const Details = ({
   const ref = useRef(null);
   return (
     <li ref={ref}
-      className="my-8 first:mt-0 last:mb-0 w-[60] mx-auto
-  flex flex-col items-center justify-between"
+      className="my-4 md:my-8 first:mt-0 last:mb-0 w-[60] mx-auto
+      flex flex-col items-center justify-between bg-black/10 rounded-lg p-2 md:p-8
+      backdrop-blur-sm border-l-4 border-gray-500"
     >
       <ListBullet reference={ref}/>
       <motion.div variants={fadeIn("up", 0.25)}
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: true, amount: 0.7 }}
-          transition={{ duration: 1.5 }} className="bg-black/100 rounded-lg p-8">
-        <h3 className="capitalize font-bold text-2xl text-white">
+          transition={{ duration: 1.5 }} className="rounded-lg p-4 md:p-8">
+        <h3 className="font-extrabold text-xl md:text-2xl text-[#c5283d]">
           {position}&nbsp;<a href={companyLink}
           target="_blank"
-          className="text-[#bf0603] capitalize">@{company}</a>
+          className="text-cyan-600 capitalize text-base">@{company}</a>
         </h3>
-        <span className="capitalize font-medium text-slate-200">
+        <span className="capitalize font-medium dark:text-[#fff0f3] text-sm md:text-base
+        text-slate-800">
           {time} | {address}
         </span>
-        <p className="font-medium w-full text-slate-400 mt-4">{work}</p>
+        <p className="font-medium w-full dark:text-slate-300 mt-4 text-sm md:text-base
+        text-slate-700">{work}</p>
       </motion.div>
     </li>
   );
@@ -52,10 +55,11 @@ const Experience = () => {
   });
   return (
     <div className="mt-20 pb-20">
-      <h1 className="text-5xl md:text-8xl font-extrabold text-center mb-10">Experience</h1>
+      <h1 className="text-4xl md:text-6xl font-extrabold text-center mb-10
+      tracking-wide">Experience</h1>
       <div className="w-[80%] md:w-[45%] mx-auto relative" ref={ref}>
         <motion.div style={{scaleY: scrollYProgress}}
-        className="absolute -left-5 md:-left-[92px] top-0 w-[4px] h-full origin-top bg-slate-800"/>
+        className="absolute -left-5 md:-left-[92px] top-10 w-[4px] h-full origin-top bg-red-800"/>
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
             position="IT Intern | Web Application Developer"
